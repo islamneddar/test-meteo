0c19a3d0eaaf911ad5ea7f4a474e174b2b35d22a
Islam Neddar <islam.neddar@gmail.com>
2023-12-02 18:20:55 +0100

    add pagianton

--- back/src/domain/city/city.controller.js
+++ back/src/domain/city/city.controller.js
@@ -9,13 +9,42 @@ export class CityController {
 
   async getCities(req, res, next){
     try{
-      const cities = await this.cityService.getCities();
-      res.status(statusResponse.STATUS_OK).json(cities);
+      const page  = parseInt(req.query.page);
+      let take  = parseInt(req.query.take);
+
+      if (!page || !take) {
+        return res.status(statusResponse.BAD_REQUEST).send({
+          error: "Missing page or take",
+        });
+      }
+      if(take > 20) take = 20;
+
+
+      console.log("getCitiesByPagination", page, take)
+      const cities = await this.cityService.getCitiesByPagination({ page, take });
+      console.log("getCitiesCOunts")
+      const citiesCount = await this.cityService.getCitiesCount();
+      const hasNext =  (page * take) < citiesCount;
+      const hasPrevious = page > 1;
+      res.status(statusResponse.STATUS_OK).json({
+        hasNext,
+        hasPrevious,
+        total : citiesCount,
+        cities : cities.map((city) => {
+          return {
+            name : city.name,
+            population : city.population,
+            insee : city.insee,
+          }
+        })
+      });
     }catch (error) {
       console.log("error", error)
       next(error)
     }
   }
+
+
 
   async getForecastForACity(req, res, next){
     try{
--- back/src/domain/city/city.service.js
+++ back/src/domain/city/city.service.js
@@ -11,4 +11,16 @@ export class CityService {
     return await this.database.all("select * from forecast where insee = ?", [id]);
   }
 
+  async getCitiesByPagination({ page, take }) {
+    console.log(typeof page, typeof take, page, take)
+    const offset = (page - 1) * take;
+    return await this.database.all("select * from city LIMIT $limit OFFSET $offset", {
+      $limit: take,
+      $offset: offset
+    });
+  }
+
+  async getCitiesCount() {
+    return await this.database.get("select count(*) from city");
+  }
 }
